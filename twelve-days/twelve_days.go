6e8d6a92d57b1cf3ec8015fa4a5c27f5b98db80e
Sam Guo <netonion@hotmail.com>
2017-11-11 19:38:57 -0500

    Twelve days v2

--- twelve-days/twelve_days.go
+++ twelve-days/twelve_days.go
@@ -1,33 +1,15 @@
 package twelve
 
-func Verse(n int) (verse string) {
-	switch n {
-	case 1:
-		verse = "On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree."
-	case 2:
-		verse = "On the second day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree."
-	case 3:
-		verse = "On the third day of Christmas my true love gave to me, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-	case 4:
-		verse = "On the fourth day of Christmas my true love gave to me, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-	case 5:
-		verse = "On the fifth day of Christmas my true love gave to me, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-	case 6:
-		verse = "On the sixth day of Christmas my true love gave to me, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-	case 7:
-		verse = "On the seventh day of Christmas my true love gave to me, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-	case 8:
-		verse = "On the eighth day of Christmas my true love gave to me, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-	case 9:
-		verse = "On the ninth day of Christmas my true love gave to me, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-	case 10:
-		verse = "On the tenth day of Christmas my true love gave to me, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-	case 11:
-		verse = "On the eleventh day of Christmas my true love gave to me, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-	case 12:
-		verse = "On the twelfth day of Christmas my true love gave to me, twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-	}
-	return
+import (
+	"fmt"
+	"strings"
+)
+
+var nth = []string{"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"}
+var obj = []string{"twelve Drummers Drumming,", "eleven Pipers Piping,", "ten Lords-a-Leaping,", "nine Ladies Dancing,", "eight Maids-a-Milking,", "seven Swans-a-Swimming,", "six Geese-a-Laying,", "five Gold Rings,", "four Calling Birds,", "three French Hens,", "two Turtle Doves, and", "a Partridge in a Pear Tree."}
+
+func Verse(n int) string {
+	return fmt.Sprintf("On the %s day of Christmas my true love gave to me, %s", nth[n-1], strings.Join(obj[12-n:], " "))
 }
 
 func Song() (song string) {

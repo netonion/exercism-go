7b24e4afd7efb78d56f0637f3ddc11c569c625c9
Sam Guo <netonion@hotmail.com>
2017-11-23 22:40:56 -0500

    Flatten array with type switch

--- flatten-array/flatten.go
+++ flatten-array/flatten.go
@@ -1,21 +1,15 @@
 package flatten
 
-import (
-	"reflect"
-)
-
 // Flatten nested slices recursively
 func Flatten(i interface{}) []interface{} {
 	res := []interface{}{}
-	s := i.([]interface{})
-	for _, item := range s {
-		if reflect.TypeOf(item) != nil {
-			if reflect.TypeOf(item).Kind() == reflect.Slice {
-				res = append(res, Flatten(item)...)
-			} else {
-				res = append(res, item)
-			}
+	switch t := i.(type) {
+	case []interface{}:
+		for _, item := range t {
+			res = append(res, Flatten(item)...)
 		}
+	case interface{}:
+		res = append(res, t)
 	}
 	return res
 }
